a4d0511faa35b4487f4d91164e4c188d44d90eff
govea <ctgovea@gmail.com>
2018-11-29 18:34:41 -0600

    production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,30 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  devtool: 'source-map',
+  entry: [path.resolve(__dirname, 'src/index')],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: false,
+      noInfo: true
+    }),
+    // TODO Eliminate duplicate packages when generating bundle
+    // new webpack.optimize.DedupePlugin(),
+    // TODO Minify JS
+    // webpack.optimize.minimize
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader'] },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] }
+    ]
+  }
+};
